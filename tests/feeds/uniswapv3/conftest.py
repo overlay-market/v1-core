ebd8cb32b337b86c3c7cddeb8d91729ccd364127
bob431136 <96485257+bob431136@users.noreply.github.com>
2021-12-22 18:20:49 -0500

    add uniswapv3 feed conftest tests

--- tests/feeds/uniswapv3/conftest.py
+++ tests/feeds/uniswapv3/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from brownie import Contract, OverlayV1Token, OverlayV1UniswapV3Feed
+from brownie import Contract, OverlayV1UniswapV3Feed
 
 
 @pytest.fixture(scope="module")
@@ -33,8 +33,8 @@ def weth():
 
 
 @pytest.fixture(scope="module")
-def ens():
-    yield Contract.from_explorer("0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72")
+def uni():
+    yield Contract.from_explorer("0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984")
 
 
 @pytest.fixture(scope="module")
@@ -43,53 +43,34 @@ def pool_daiweth_30bps():
 
 
 @pytest.fixture(scope="module")
-def pool_ensweth_30bps():
+def pool_uniweth_30bps():
     # to be used as example ovlweth pool
-    yield Contract.from_explorer("0x92560C178cE069CC014138eD3C2F5221Ba71f58a")
-
-
-@pytest.fixture(scope="module", params=[8000000])
-def create_token(gov, alice, bob, request):
-    sup = request.param
-
-    def create_token(supply=sup):
-        tok = gov.deploy(OverlayV1Token)
-        tok.mint(gov, supply * 10 ** tok.decimals(), {"from": gov})
-        tok.transfer(alice, (supply/2) * 10 ** tok.decimals(), {"from": gov})
-        tok.transfer(bob, (supply/2) * 10 ** tok.decimals(), {"from": gov})
-        return tok
-
-    yield create_token
-
-
-@pytest.fixture(scope="module")
-def token(create_token):
-    yield create_token()
+    yield Contract.from_explorer("0x1d42064Fc4Beb5F8aAF85F4617AE8b3b5B8Bd801")
 
 
 @pytest.fixture(scope="module", params=[(600, 3600)])
-def create_quanto_feed(gov, token, pool_daiweth_30bps, pool_ensweth_30bps,
-                       dai, weth, ens, request):
-    micro, macro, p, r = request.param
+def create_quanto_feed(gov, pool_daiweth_30bps, pool_uniweth_30bps,
+                       dai, weth, uni, request):
+    micro, macro = request.param
 
     mkt_pool = pool_daiweth_30bps.address
-    oe_pool = pool_ensweth_30bps.address
-    tok = token.address
+    oe_pool = pool_uniweth_30bps.address
+    tok = uni.address
     mkt_base_tok = weth.address
     mkt_quote_tok = dai.address
     mkt_base_amt = 1 * 10 ** weth.decimals()
 
-    def create_feed(market_pool=mkt_pool, ovlweth_pool=oe_pool, ovl=tok,
-                    market_base_token=mkt_base_tok,
-                    market_quote_token=mkt_quote_tok,
-                    market_base_amount=mkt_base_amt, micro_window=micro,
-                    macro_window=macro):
+    def create_quanto_feed(market_pool=mkt_pool, ovlweth_pool=oe_pool, ovl=tok,
+                           market_base_token=mkt_base_tok,
+                           market_quote_token=mkt_quote_tok,
+                           market_base_amount=mkt_base_amt, micro_window=micro,
+                           macro_window=macro):
         feed = gov.deploy(OverlayV1UniswapV3Feed, market_pool, ovlweth_pool,
                           ovl, market_base_token, market_quote_token,
                           market_base_amount, micro_window, macro_window)
         return feed
 
-    yield create_feed
+    yield create_quanto_feed
 
 
 @pytest.fixture(scope="module")
@@ -98,30 +79,30 @@ def quanto_feed(create_quanto_feed):
 
 
 @pytest.fixture(scope="module", params=[(600, 3600)])
-def create_inverse_feed(gov, token, pool_ensweth_30bps, weth, ens, request):
-    micro, macro, p, r = request.param
+def create_inverse_feed(gov, pool_uniweth_30bps, weth, uni, request):
+    micro, macro = request.param
 
-    # treating ens as ovl for testing
-    mkt_pool = pool_ensweth_30bps.address
-    oe_pool = pool_ensweth_30bps.address
-    tok = token.address
+    # treating uni as ovl for testing
+    mkt_pool = pool_uniweth_30bps.address
+    oe_pool = pool_uniweth_30bps.address
+    tok = uni.address
     mkt_base_tok = weth.address
-    mkt_quote_tok = ens.address
-    mkt_base_amt = 1 * 10 ** ens.decimals()
+    mkt_quote_tok = uni.address
+    mkt_base_amt = 1 * 10 ** uni.decimals()
 
-    def create_feed(market_pool=mkt_pool, ovlweth_pool=oe_pool, ovl=tok,
-                    market_base_token=mkt_base_tok,
-                    market_quote_token=mkt_quote_tok,
-                    market_base_amount=mkt_base_amt, micro_window=micro,
-                    macro_window=macro):
+    def create_inverse_feed(market_pool=mkt_pool, ovlweth_pool=oe_pool,
+                            ovl=tok, market_base_token=mkt_base_tok,
+                            market_quote_token=mkt_quote_tok,
+                            market_base_amount=mkt_base_amt,
+                            micro_window=micro, macro_window=macro):
         feed = gov.deploy(OverlayV1UniswapV3Feed, market_pool, ovlweth_pool,
                           ovl, market_base_token, market_quote_token,
                           market_base_amount, micro_window, macro_window)
         return feed
 
-    yield create_feed
+    yield create_inverse_feed
 
 
 @pytest.fixture(scope="module")
 def inverse_feed(create_inverse_feed):
-    yield create_quanto_feed()
+    yield create_inverse_feed()
---
+++ tests/feeds/uniswapv3/test_conftest.py
@@ -0,0 +1,46 @@
+def test_token_fixtures(dai, weth, uni):
+    assert dai.name() == "Dai Stablecoin"
+    assert weth.name() == "Wrapped Ether"
+    assert uni.name() == "Uniswap"
+
+
+def test_pool_fixtures(dai, weth, uni, pool_daiweth_30bps, pool_uniweth_30bps):
+    assert pool_daiweth_30bps.fee() == 3000
+    assert pool_daiweth_30bps.token0() == dai
+    assert pool_daiweth_30bps.token1() == weth
+
+    assert pool_uniweth_30bps.fee() == 3000
+    assert pool_uniweth_30bps.token0() == uni
+    assert pool_uniweth_30bps.token1() == weth
+
+
+def test_quanto_feed_fixture(dai, weth, uni, pool_daiweth_30bps,
+                             pool_uniweth_30bps, quanto_feed):
+    assert quanto_feed.marketPool() == pool_daiweth_30bps
+    assert quanto_feed.ovlWethPool() == pool_uniweth_30bps
+    assert quanto_feed.ovl() == uni
+
+    assert quanto_feed.marketToken0() == dai
+    assert quanto_feed.marketToken1() == weth
+    assert quanto_feed.ovlWethToken0() == uni
+    assert quanto_feed.ovlWethToken1() == weth
+
+    assert quanto_feed.marketBaseToken() == weth
+    assert quanto_feed.marketQuoteToken() == dai
+    assert quanto_feed.marketBaseAmount() == 1 * 10 ** 18
+
+
+def test_inverse_feed_fixture(dai, weth, uni, pool_daiweth_30bps,
+                              pool_uniweth_30bps, inverse_feed):
+    assert inverse_feed.marketPool() == pool_uniweth_30bps
+    assert inverse_feed.ovlWethPool() == pool_uniweth_30bps
+    assert inverse_feed.ovl() == uni
+
+    assert inverse_feed.marketToken0() == uni
+    assert inverse_feed.marketToken1() == weth
+    assert inverse_feed.ovlWethToken0() == uni
+    assert inverse_feed.ovlWethToken1() == weth
+
+    assert inverse_feed.marketBaseToken() == weth
+    assert inverse_feed.marketQuoteToken() == uni
+    assert inverse_feed.marketBaseAmount() == 1 * 10 ** 18
