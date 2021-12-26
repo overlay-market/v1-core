9aedc1c23712e89f0b45a655f77573ab451d3f6c
bob431136 <96485257+bob431136@users.noreply.github.com>
2021-12-26 10:19:48 -0500

    refactor market build test, add test build adds oi

--- tests/markets/test_build.py
+++ tests/markets/test_build.py
@@ -3,12 +3,28 @@ from brownie.test import given, strategy
 from decimal import Decimal
 
 
+def calculate_position_info(oi: Decimal,
+                            leverage: Decimal,
+                            trading_fee_rate: Decimal,
+                            lmbda: Decimal,
+                            cap_oi: Decimal) -> (Decimal, Decimal, Decimal):
+    collateral = oi / leverage
+    impact_fee = 0  # TODO: add in
+    trade_fee = oi * trading_fee_rate
+
+    collateral_adjusted = collateral - impact_fee - trade_fee
+    oi_adjusted = collateral_adjusted * leverage
+    debt = oi_adjusted - collateral_adjusted
+
+    return collateral_adjusted, oi_adjusted, debt
+
+
 @given(
     oi=strategy('decimal', min_value='0.001', max_value='800000'),
     leverage=strategy('decimal', min_value='1.0', max_value='5.0'),
     is_long=strategy('bool'))
-def test_build_creates_position(market, feed, ovl, alice,
-                                oi, leverage, is_long):
+def test_build_creates_position(market, feed, ovl, alice, oi, leverage,
+                                is_long):
     expect_pos_id = market.nextPositionId()
 
     input_collateral = int(oi / leverage * Decimal(1e18))
@@ -26,13 +42,12 @@ def test_build_creates_position(market, feed, ovl, alice,
     assert actual_pos_id == expect_pos_id
 
     # calculate expected pos info data
-    collateral = oi / leverage
-    impact_fee = 0  # TODO: add in
-    trade_fee = oi * Decimal(market.tradingFeeRate() / 1e18)
-
-    collateral_adjusted = collateral - impact_fee - trade_fee
-    oi_adjusted = collateral_adjusted * leverage
-    debt = oi_adjusted - collateral_adjusted
+    trading_fee_rate = Decimal(market.tradingFeeRate() / 1e18)
+    lmbda = Decimal(market.lmbda() / 1e18)
+    cap_oi = Decimal(market.capOi() / 1e18)
+    (collateral_adjusted, oi_adjusted, debt) \
+        = calculate_position_info(oi, leverage, trading_fee_rate, lmbda,
+                                  cap_oi)
 
     # calculate expected entry price
     # NOTE: ask(), bid() tested in test_price.py
@@ -58,3 +73,42 @@ def test_build_creates_position(market, feed, ovl, alice,
     assert int(actual_oi_shares) == approx(expect_oi_shares, rel=1e-4)
     assert int(actual_debt) == approx(expect_debt, rel=1e-4)
     assert int(actual_cost) == approx(expect_cost, rel=1e-4)
+
+
+@given(
+    oi=strategy('decimal', min_value='0.001', max_value='800000'),
+    leverage=strategy('decimal', min_value='1.0', max_value='5.0'),
+    is_long=strategy('bool'))
+def test_build_adds_oi(market, feed, ovl, alice, oi, leverage, is_long):
+    input_collateral = int(oi / leverage * Decimal(1e18))
+    input_leverage = int(leverage * Decimal(1e18))
+    input_is_long = is_long
+    input_min_oi = 0  # NOTE: testing for min_oi below
+
+    # priors actual values
+    _ = market.update({"from": alice})  # update funding prior
+    expect_oi = market.oiLong() if is_long else market.oiShort()
+    expect_oi_shares = market.oiLongShares() \
+        if is_long else market.oiShortShares()
+
+    # approve market for spending then build
+    ovl.approve(market, input_collateral, {"from": alice})
+    _ = market.build(input_collateral, input_leverage, input_is_long,
+                     input_min_oi, {"from": alice})
+
+    # calculate expected oi info data
+    trading_fee_rate = Decimal(market.tradingFeeRate() / 1e18)
+    lmbda = Decimal(market.lmbda() / 1e18)
+    cap_oi = Decimal(market.capOi() / 1e18)
+    _, oi_adjusted, _ = calculate_position_info(oi, leverage,
+                                                trading_fee_rate, lmbda,
+                                                cap_oi)
+    expect_oi += int(oi_adjusted * Decimal(1e18))
+    expect_oi_shares += int(oi_adjusted * Decimal(1e18))
+
+    # compare with actual aggregate oi values
+    actual_oi = market.oiLong() if is_long else market.oiShort()
+    actual_oi_shares = market.oiLongShares() if is_long else market.oiShort()
+
+    assert int(actual_oi) == approx(expect_oi, rel=1e-4)
+    assert int(actual_oi_shares) == approx(expect_oi_shares, rel=1e-4)
