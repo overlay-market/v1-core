87928232979acf3abc77fa2b727541e4a6ab7273
bob431136 <96485257+bob431136@users.noreply.github.com>
2022-02-07 23:06:48 -0500

    Remove access import from factory

--- contracts/OverlayV1Factory.sol
+++ contracts/OverlayV1Factory.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-
 import "./interfaces/IOverlayV1FeedFactory.sol";
 import "./libraries/Risk.sol";
 
