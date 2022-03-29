ca855b809fc3367b35d8f10f148986ca1bf31010
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2022-03-29 12:06:15 -0400

    Update Balancer fixed point lib forks

--- contracts/libraries/FixedPoint.sol
+++ contracts/libraries/FixedPoint.sol
@@ -13,10 +13,16 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 pragma solidity 0.8.10;
+
 import "./LogExpMath.sol";
+import "../utils/Errors.sol";
+
+/* solhint-disable private-vars-leading-underscore */
 
 library FixedPoint {
     uint256 internal constant ONE = 1e18; // 18 decimal places
+    uint256 internal constant TWO = 2 * ONE;
+    uint256 internal constant FOUR = 4 * ONE;
     uint256 internal constant MAX_POW_RELATIVE_ERROR = 10000; // 10^(-14)
 
     // Minimum base for the power function when the exponent is 'free' (larger than ONE).
@@ -82,39 +88,68 @@ library FixedPoint {
         }
     }
 
-    /// @dev Returns x^y, assuming both are fixed point numbers, rounding down. The result is
-    /// @dev guaranteed to not be above the true value (that is, the error function expected -
-    /// @dev actual is always positive).
+    /**
+     * @dev Returns x^y, assuming both are fixed point numbers, rounding down. The result is guaranteed to not be above
+     * the true value (that is, the error function expected - actual is always positive).
+     */
     function powDown(uint256 x, uint256 y) internal pure returns (uint256) {
-        if (0 == y || x == ONE) return ONE;
-        else if (x == 0) return 0;
-
-        uint256 raw = LogExpMath.pow(x, y);
-        uint256 maxError = add(mulUp(raw, MAX_POW_RELATIVE_ERROR), 1);
-
-        if (raw < maxError) {
+        // Optimize for when y equals 1.0, 2.0 or 4.0, as those are very simple to implement and occur often in 50/50
+        // and 80/20 Weighted Pools
+        if (0 == y || x == ONE) {
+            return ONE;
+        } else if (x == 0) {
             return 0;
+        } else if (y == ONE) {
+            return x;
+        } else if (y == TWO) {
+            return mulDown(x, x);
+        } else if (y == FOUR) {
+            uint256 square = mulDown(x, x);
+            return mulDown(square, square);
         } else {
-            return sub(raw, maxError);
+            uint256 raw = LogExpMath.pow(x, y);
+            uint256 maxError = add(mulUp(raw, MAX_POW_RELATIVE_ERROR), 1);
+
+            if (raw < maxError) {
+                return 0;
+            } else {
+                return sub(raw, maxError);
+            }
         }
     }
 
-    /// @dev Returns x^y, assuming both are fixed point numbers, rounding up. The result is
-    /// @dev guaranteed to not be below the true value (that is, the error function expected -
-    /// @dev actual is always negative).
+    /**
+     * @dev Returns x^y, assuming both are fixed point numbers, rounding up. The result is guaranteed to not be below
+     * the true value (that is, the error function expected - actual is always negative).
+     */
     function powUp(uint256 x, uint256 y) internal pure returns (uint256) {
-        if (ONE == x || y == 0) return ONE;
-        else if (x == 0) return 0;
+        // Optimize for when y equals 1.0, 2.0 or 4.0, as those are very simple to implement and occur often in 50/50
+        // and 80/20 Weighted Pools
+        if (0 == y || x == ONE) {
+            return ONE;
+        } else if (x == 0) {
+            return 0;
+        } else if (y == ONE) {
+            return x;
+        } else if (y == TWO) {
+            return mulUp(x, x);
+        } else if (y == FOUR) {
+            uint256 square = mulUp(x, x);
+            return mulUp(square, square);
+        } else {
+            uint256 raw = LogExpMath.pow(x, y);
+            uint256 maxError = add(mulUp(raw, MAX_POW_RELATIVE_ERROR), 1);
 
-        uint256 raw = LogExpMath.pow(x, y);
-        uint256 maxError = add(mulUp(raw, MAX_POW_RELATIVE_ERROR), 1);
-
-        return add(raw, maxError);
+            return add(raw, maxError);
+        }
     }
 
-    /// @dev Returns the complement of a value (1 - x), capped to 0 if x is larger than 1.
-    /// @dev Useful when computing the complement for values with some level of relative error, as
-    /// @dev it strips this error and prevents intermediate negative values.
+    /**
+     * @dev Returns the complement of a value (1 - x), capped to 0 if x is larger than 1.
+     *
+     * Useful when computing the complement for values with some level of relative error, as it strips this error and
+     * prevents intermediate negative values.
+     */
     function complement(uint256 x) internal pure returns (uint256) {
         return (x < ONE) ? (ONE - x) : 0;
     }
