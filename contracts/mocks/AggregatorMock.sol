a541f5568f96ee405ac6ba1f357a0296fed5f2a6
Parashar <ankitabc12@gmail.com>
2022-10-18 20:31:54 +0530

    fixing lint issue

--- contracts/mocks/AggregatorMock.sol
+++ contracts/mocks/AggregatorMock.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.10;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract AggregatorMock is AggregatorV3Interface {
-    
     struct RoundData {
         int256 answer;
         uint256 startedAt;
