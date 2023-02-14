a9e19b11e2450d4b0421f2e4f816eb66e789d888
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-14 10:33:04 -0800

    add debug-test-python.yaml file

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,11 +6,11 @@ from hexbytes import HexBytes
 
 # governance multisig
 # TODO: change
-GOV = "0x95f972fc4D17a0D343Cd5eaD8d6DCBef5606CA66"
+GOV = "0x8e8b3e19717A5DDCfccce9Bf3b225E61efDD7937"
 
 # fee recipient address
 # TODO: change
-FEE_RECIPIENT = "0xDFafdfF09C1d63257892A8d2F56483588B99315A"
+FEE_RECIPIENT = "0x8e8b3e19717A5DDCfccce9Bf3b225E61efDD7937"
 
 # ROLES
 ADMIN = "ADMIN"
@@ -24,37 +24,40 @@ def _role(name):
         return HexBytes("0x00")
     return web3.solidityKeccak(['string'], [name])
 
+def main(): 
+  click.echo(_role(MINTER))
 
-def main():
-    """
-    Deploys new OverlayV1Token and OverlayV1Factory contracts.
+# def main():
+#     """
+#     Deploys new OverlayV1Token and OverlayV1Factory contracts.
 
-    Grants token admin rights to the factory to enable deploying of markets
-    with mint and burn priveleges. Grants token admin rights to governor and
-    renounces all token rights for deployer.
-    """
-    click.echo(f"You are using the '{network.show_active()}' network")
-    dev = accounts.load(click.prompt(
-        "Account", type=click.Choice(accounts.load())))
+#     Grants token admin rights to the factory to enable deploying of markets
+#     with mint and burn priveleges. Grants token admin rights to governor and
+#     renounces all token rights for deployer.
+#     """
+    # click.echo(f"You are using the '{network.show_active()}' network")
+    # dev = accounts.load(click.prompt(
+    #     "Account", type=click.Choice(accounts.load())))
 
-    # deploy OVL
-    ovl = OverlayV1Token.deploy({"from": dev}, publish_source=True)
-    click.echo(f"OVL Token deployed [{ovl.address}]")
+    # # deploy OVL
+    # # ovl = OverlayV1Token.deploy({"from": dev}, publish_source=True)
+    # ovl = OverlayV1Token.deploy({"from": dev}, publish_source=True)
+    # click.echo(f"OVL Token deployed [{ovl.address}]")
 
-    # deploy market factory
-    factory = OverlayV1Factory.deploy(
-        ovl, FEE_RECIPIENT, {"from": dev}, publish_source=True)
-    click.echo(f"Factory deployed [{factory.address}]")
+    # # deploy market factory
+    # factory = OverlayV1Factory.deploy(
+    #     ovl, FEE_RECIPIENT, {"from": dev}, publish_source=True)
+    # click.echo(f"Factory deployed [{factory.address}]")
 
-    # grant market factory admin role to grant minter + burner roles to markets
-    # on deployMarket calls
-    ovl.grantRole(_role(ADMIN), factory, {"from": dev})
+    # # grant market factory admin role to grant minter + burner roles to markets
+    # # on deployMarket calls
+    # ovl.grantRole(_role(ADMIN), factory, {"from": dev})
 
-    # grant admin rights to gov
-    ovl.grantRole(_role(ADMIN), GOV, {"from": dev})
-    ovl.grantRole(_role(MINTER), GOV, {"from": dev})
-    ovl.grantRole(_role(GOVERNOR), GOV, {"from": dev})
-    click.echo(f"OVL Token roles granted to [{GOV}]")
+    # # grant admin rights to gov
+    # ovl.grantRole(_role(ADMIN), GOV, {"from": dev})
+    # ovl.grantRole(_role(MINTER), GOV, {"from": dev})
+    # ovl.grantRole(_role(GOVERNOR), GOV, {"from": dev})
+    # click.echo(f"OVL Token roles granted to [{GOV}]")
 
     # renounce admin rights so only gov has roles
-    ovl.renounceRole(_role(ADMIN), dev, {"from": dev})
+    # ovl.renounceRole(_role(ADMIN), dev, {"from": dev})
