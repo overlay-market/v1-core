28fcf4cce8005ab291c27fe352ae106a14a69d13
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-16 14:33:38 -0800

    Update import name

--- scripts/feeds/chainlink/deploy.py
+++ scripts/feeds/chainlink/deploy.py
@@ -1,11 +1,11 @@
 import click
 
-from brownie import OverlayV1ChainlinkFactory, accounts, network
+from brownie import OverlayV1ChainlinkFeedFactory, accounts, network
 
 
 def main():
     """
-    Deploys a new OverlayV1ChainlinkFactory contract, which allows for
+    Deploys a new OverlayV1ChainlinkFeedFactory contract, which allows for
     permissionless deployment of OverlayV1ChainlinkFeed feeds.
     """
     click.echo(f"You are using the '{network.show_active()}' network")
@@ -20,7 +20,7 @@ def main():
     ]
 
     # deploy feed factory
-    feed_factory = OverlayV1ChainlinkFactory.deploy(
+    feed_factory = OverlayV1ChainlinkFeedFactory.deploy(
         *args, {"from": dev}, publish_source=True)
     click.echo(f"Chainlink Feed Factory deployed [{feed_factory.address}]")
     click.echo("NOTE: Feed Factory is not registered in OverlayV1Factory yet!")
