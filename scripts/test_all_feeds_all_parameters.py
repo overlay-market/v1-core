ccc7c03e67a647affb3c35e9f99d0c1878576f81
dryad <67245619+dryad@users.noreply.github.com>
2023-03-18 14:32:06 -0600

    fixed typo

--- scripts/test_all_feeds_all_parameters.py
+++ scripts/test_all_feeds_all_parameters.py
@@ -3,10 +3,10 @@ from all_feeds_all_parameters import AllFeedsAllParameters
 
 class TestRiskParams(unittest.TestCase):
 
-    def test_risk_param_order(self):
+    def test_risk_param_order(self): #the order is important, so we test it
         risk_params = ["k", "lambda", "delta", "capPayoff", "capNotional", "capLeverage", "circuitBreakerWindow", "circuitBreakerMintTarget", "maintenanceMarginFraction", "maintenanceMarginBurnRate", "liquidationFeeRate", "tradingFeeRate", "minCollateral", "priceDriftUpperLimit", "averageBlockTime"]
         afap = AllFeedsAllParameters
-        assertListEqual(afap.risk_params, risk_params)
+        self.assertListEqual(afap.risk_params, risk_params)
 
         ...
 
