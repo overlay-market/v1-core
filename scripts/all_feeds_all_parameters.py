cbd40ae3c1407bfcc879e53795b869b4715d578d
mcillay <a@b.c>
2023-03-19 18:08:00 -0600

    added symbolic constants and oracle in dict of dict of dicts

--- scripts/all_feeds_all_parameters.py
+++ scripts/all_feeds_all_parameters.py
@@ -4,13 +4,43 @@
 
 class AllFeedsAllParameters:
 
+	#CHAINS:
+	ETH_MAIN = 'ethereum_goerli'
+	ETH_TEST = 'ethereum_mainnet'
+	ARB_TEST = 'arbitrum_goerli'
+	ARB_MAIN = 'arbitrum_one'
+
     ## XXX these are ordered!!! XXX DO NOT CHANGE
 	risk_params = ["k", "lambda", "delta", "capPayoff", "capNotional", "capLeverage", "circuitBreakerWindow", "circuitBreakerMintTarget", "maintenanceMarginFraction", "maintenanceMarginBurnRate", "liquidationFeeRate", "tradingFeeRate", "minCollateral", "priceDriftUpperLimit", "averageBlockTime"]
 
 	@classmethod
-	def filter_by_network(cls, selected_networks: list):
-		afap = cls.AllFeedsAllParameters
-		return  {market: {network: c for network, c in b.items() if network in selected_networks} for market, b in a.items()}
+	def filter_by_blockchain(cls, selected_chains: list):
+		afap = cls.all_feeds_all_parameters
+		res = {} 
+		for market, chain in afap.items():
+			for chain, oracle in chain.items():
+				if chain in selected_chains:
+					if market not in res.keys():
+						res[market] = {}
+					for oracle, params in oracle.items():					
+						res[market].update({chain: {oracle: params}})
+
+		return res
+
+	@classmethod
+	def filter_by_oracle(cls, selected_oracles: list):
+		afap = cls.all_feeds_all_parameters
+		res = {} 
+		for market, chain in afap.items():
+			for chain, oracle in chain.items():
+				for oracle, params in oracle.items():
+					if oracle in selected_oracles:
+						if market in res.keys():
+							res[market].update({chain: {oracle: params}})
+						else:
+							res[market] = {chain: {oracle: params}}
+		return res
+					
 
 	@classmethod
 	def risk_param_array(cls, params):
@@ -32,53 +62,57 @@ class AllFeedsAllParameters:
 	all_feeds_all_parameters = {
 		"mcap1000" :  {
 			"arbitrum_goerli" : {
-				"macroWindow" : 3600,
-				"microWindow" : 600,
-				"aggregator"  : "0x2748A42aBd328835DFDA748bdD1D77Ce3c3312EE",
-				"factory_address" : "0x6eD5CCC86F24AA307dee58d4630d97F2f660C38A",
-				"chainlink_feed_factory_contract_address" : "0x5121C75e7cADE920B32FeCf230600654F73ee73C",
-				"risk_parameters": {
-					"k": 115740740741,
-					"lambda": 613091000000000000,
-					"delta": 3510000000000000,
-					"capPayoff": 10000000000000000000,
-					"capNotional": 800000000000000000000000,
-					"capLeverage": 5000000000000000000,
-					"circuitBreakerWindow": 2592000,
-					"circuitBreakerMintTarget": 66670000000000000000000,
-					"maintenanceMarginFraction": 57790000000000000,
-					"maintenanceMarginBurnRate": 52968000000000000,
-					"liquidationFeeRate": 50000000000000000,
-					"tradingFeeRate": 750000000000000,
-					"minCollateral": 100000000000000,
-					"priceDriftUpperLimit": 100000000000000,
-					"averageBlockTime": 0
+				"translucent": {
+					"macroWindow" : 3600,
+					"microWindow" : 600,
+					"aggregator"  : "0x2748A42aBd328835DFDA748bdD1D77Ce3c3312EE",
+					"factory_address" : "0x6eD5CCC86F24AA307dee58d4630d97F2f660C38A",
+					"chainlink_feed_factory_contract_address" : "0x5121C75e7cADE920B32FeCf230600654F73ee73C",
+					"risk_parameters": {
+						"k": 115740740741,
+						"lambda": 613091000000000000,
+						"delta": 3510000000000000,
+						"capPayoff": 10000000000000000000,
+						"capNotional": 800000000000000000000000,
+						"capLeverage": 5000000000000000000,
+						"circuitBreakerWindow": 2592000,
+						"circuitBreakerMintTarget": 66670000000000000000000,
+						"maintenanceMarginFraction": 57790000000000000,
+						"maintenanceMarginBurnRate": 52968000000000000,
+						"liquidationFeeRate": 50000000000000000,
+						"tradingFeeRate": 750000000000000,
+						"minCollateral": 100000000000000,
+						"priceDriftUpperLimit": 100000000000000,
+						"averageBlockTime": 0
+						}
 					}
 				},
 			"arbitrum_one" : {
-				"macroWindow" : 3600,
-				"microWindow" : 600,
-				"aggregator"  : "0x2748A42aBd328835DFDA748bdD1D77Ce3c3312EE",
-				"factory_address" : "0x6eD5CCC86F24AA307dee58d4630d97F2f660C38A",
-				"chainlink_feed_factory_contract_address" : "0x5121C75e7cADE920B32FeCf230600654F73ee73C",
-				"risk_parameters": {
-					"k": 115740740741,
-					"lambda": 613091000000000000,
-					"delta": 3510000000000000,
-					"capPayoff": 10000000000000000000,
-					"capNotional": 800000000000000000000000,
-					"capLeverage": 5000000000000000000,
-					"circuitBreakerWindow": 2592000,
-					"circuitBreakerMintTarget": 66670000000000000000000,
-					"maintenanceMarginFraction": 57790000000000000,
-					"maintenanceMarginBurnRate": 52968000000000000,
-					"liquidationFeeRate": 50000000000000000,
-					"tradingFeeRate": 750000000000000,
-					"minCollateral": 100000000000000,
-					"priceDriftUpperLimit": 100000000000000,
-					"averageBlockTime": 0
+				"translucent": {
+					"macroWindow" : 3600,
+					"microWindow" : 600,
+					"aggregator"  : "0x2748A42aBd328835DFDA748bdD1D77Ce3c3312EE",
+					"factory_address" : "0x6eD5CCC86F24AA307dee58d4630d97F2f660C38A",
+					"chainlink_feed_factory_contract_address" : "0x5121C75e7cADE920B32FeCf230600654F73ee73C",
+					"risk_parameters": {
+						"k": 115740740741,
+						"lambda": 613091000000000000,
+						"delta": 3510000000000000,
+						"capPayoff": 10000000000000000000,
+						"capNotional": 800000000000000000000000,
+						"capLeverage": 5000000000000000000,
+						"circuitBreakerWindow": 2592000,
+						"circuitBreakerMintTarget": 66670000000000000000000,
+						"maintenanceMarginFraction": 57790000000000000,
+						"maintenanceMarginBurnRate": 52968000000000000,
+						"liquidationFeeRate": 50000000000000000,
+						"tradingFeeRate": 750000000000000,
+						"minCollateral": 100000000000000,
+						"priceDriftUpperLimit": 100000000000000,
+						"averageBlockTime": 0
+						}
 					}
-				}
+				},
 			},
 
 			# TODELETE toy example for testing
