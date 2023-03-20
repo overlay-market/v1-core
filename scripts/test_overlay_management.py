2b41ffeee22723f1da958e0d171ebb831b5da4c2
dryad <67245619+dryad@users.noreply.github.com>
2023-03-19 22:18:12 -0600

    test rectification

--- scripts/test_overlay_management.py
+++ scripts/test_overlay_management.py
@@ -1,22 +1,24 @@
 import unittest
-from all_feeds_all_parameters import AllFeedsAllParameters
+from overlay_management import OM 
 
 class TestRiskParams(unittest.TestCase):
 
     def test_risk_param_order(self): #the order is important, so we test it
         risk_params = ["k", "lambda", "delta", "capPayoff", "capNotional", "capLeverage", "circuitBreakerWindow", "circuitBreakerMintTarget", "maintenanceMarginFraction", "maintenanceMarginBurnRate", "liquidationFeeRate", "tradingFeeRate", "minCollateral", "priceDriftUpperLimit", "averageBlockTime"]
-        afap = AllFeedsAllParameters
-        self.assertListEqual(afap.risk_params, risk_params)
+        self.assertListEqual(OM.risk_params, risk_params)
+
+    def test_all_feeds_all_parameters(self):
+        afap = OM.get_all_feeds_all_parameters()
+        ...
 
     def test_filter_by_network(self):
-        afap = AllFeedsAllParameters
 
-        chainlist = [afap.ARB_TEST, afap.ARB_MAIN]
-        filtered = afap.filter_by_blockchain(chainlist)
+        chainlist = [OM.ARB_TEST, OM.ARB_MAIN]
+        filtered = OM.filter_by_blockchain(chainlist)
         self.assertEqual(len(filtered['mcap1000'].keys()), 2)
 
-        chainlist = [afap.ARB_TEST]
-        filtered = afap.filter_by_blockchain(chainlist)
+        chainlist = [OM.ARB_TEST]
+        filtered = OM.filter_by_blockchain(chainlist)
         self.assertEqual(len(filtered['mcap1000'].keys()), 1)
 
 
