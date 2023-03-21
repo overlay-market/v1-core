a539171005a360dccc314146f228a97d756c1a94
c-note <dongustavo20@gmail.com>
2023-03-21 16:07:43 +0100

    updated deployFeed and deployMarket to only deploy if market chain is same with netwok chain

--- scripts/feeds/chainlink/deployFeed.py
+++ scripts/feeds/chainlink/deployFeed.py
@@ -17,28 +17,29 @@ def main():
 
     for key, chain_dict in deployable_feeds.items():
         for chain_key in chain_dict:
-            feed_oracle = OM.getKey(chain_dict[chain_key])
-            if 'feed_address' not in chain_dict[chain_key][feed_oracle]:
-                parameters = OM.get_feed_network_parameters(key, chain_key, feed_oracle)
-                aggregator = parameters[0]
-                overlay_v1_chainlink_feed_factory_contract_address = parameters[3]
+            if chain_key == network.show_active():
+                feed_oracle = OM.getKey(chain_dict[chain_key])
+                if 'feed_address' not in chain_dict[chain_key][feed_oracle]:
+                    parameters = OM.get_feed_network_parameters(key, chain_key, feed_oracle)
+                    aggregator = parameters[0]
+                    overlay_v1_chainlink_feed_factory_contract_address = parameters[3]
 
-                print(aggregator,overlay_v1_chainlink_feed_factory_contract_address)
+                    print(aggregator,overlay_v1_chainlink_feed_factory_contract_address)
 
-                # connect to overlay v1 chainlink feed factory contract
-                overlay_v1_chainlink_feed_factory_contract = Contract.from_explorer(
-                    f"{overlay_v1_chainlink_feed_factory_contract_address}")
+                    # connect to overlay v1 chainlink feed factory contract
+                    overlay_v1_chainlink_feed_factory_contract = Contract.from_explorer(
+                        f"{overlay_v1_chainlink_feed_factory_contract_address}")
 
-                # get feed address
-                feed_contract_address = overlay_v1_chainlink_feed_factory_contract.deployFeed.call(
-                    aggregator, {"from": dev})
+                    # get feed address
+                    feed_contract_address = overlay_v1_chainlink_feed_factory_contract.deployFeed.call(
+                        aggregator, {"from": dev})
 
-                # deploy feed
-                click.echo("Deploying Chainlink Feed")
-                overlay_v1_chainlink_feed_factory_contract.deployFeed(
-                    aggregator, {"from": dev, "maxFeePerGas": 3674000000})
-                click.echo(f"Chainlink Feed deployed [{feed_contract_address}]")
+                    # deploy feed
+                    click.echo("Deploying Chainlink Feed")
+                    overlay_v1_chainlink_feed_factory_contract.deployFeed(
+                        aggregator, {"from": dev, "maxFeePerGas": 3674000000})
+                    click.echo(f"Chainlink Feed deployed [{feed_contract_address}]")
 
-                all_feeds_all_parameters[key][chain_key][feed_oracle]['feed_address'] = f'{feed_contract_address}'
-                data = all_feeds_all_parameters
-                OM.update_feeds_with_market_parameter(data)
+                    all_feeds_all_parameters[key][chain_key][feed_oracle]['feed_address'] = f'{feed_contract_address}'
+                    data = all_feeds_all_parameters
+                    OM.update_feeds_with_market_parameter(data)
--- scripts/feeds/chainlink/deployMarket.py
+++ scripts/feeds/chainlink/deployMarket.py
@@ -16,39 +16,40 @@ def main():
 
     for key, chain_dict in deployable_feeds.items():
         for chain_key in chain_dict:
-            feed_oracle = OM.getKey(chain_dict[chain_key])
-            if 'market' not in chain_dict[chain_key][feed_oracle]:
-                parameters = OM.get_feed_network_parameters(key, chain_key, feed_oracle)
-                aggregator = parameters[0]
-                risk_parameters = parameters[4]
-                overlay_v1_factory_address = parameters[2]
-                overlay_v1_chainlink_feed_factory_contract_address = parameters[3]
+            if chain_key == network.show_active():
+                feed_oracle = OM.getKey(chain_dict[chain_key])
+                if 'market' not in chain_dict[chain_key][feed_oracle]:
+                    parameters = OM.get_feed_network_parameters(key, chain_key, feed_oracle)
+                    aggregator = parameters[0]
+                    risk_parameters = parameters[4]
+                    overlay_factory_address = parameters[2]
+                    overlay_chainlink_feed_factory_contract_address = parameters[3]
 
-                # connect to overlay v1 chainlink feed factory contract
-                overlay_v1_chainlink_feed_factory_contract = Contract.from_explorer(
-                    f"{overlay_v1_chainlink_feed_factory_contract_address}")
+                    # connect to overlay v1 chainlink feed factory contract
+                    overlay_chainlink_feed_factory_contract = Contract.from_explorer(
+                        f"{overlay_chainlink_feed_factory_contract_address}")
 
-                # connect to overlay v1 factory contract
-                overlay_v1_factory_contract = Contract.from_explorer(
-                    f'{overlay_v1_factory_address}')
+                    # connect to overlay v1 factory contract
+                    overlay_factory_contract = Contract.from_explorer(
+                        f'{overlay_factory_address}')
 
-                # get feed address
-                feed_contract_address = overlay_v1_chainlink_feed_factory_contract.getFeed(aggregator)
+                    # get feed address
+                    feed_contract_address = overlay_chainlink_feed_factory_contract.getFeed(aggregator)
 
-                # get market address
-                market = overlay_v1_factory_contract.deployMarket.call(
-                    overlay_v1_chainlink_feed_factory_contract_address,feed_contract_address,risk_parameters,
-                    {"from": dev})
+                    # get market address
+                    market = overlay_factory_contract.deployMarket.call(
+                        overlay_chainlink_feed_factory_contract_address,feed_contract_address,risk_parameters,
+                        {"from": dev})
 
-                # deploy market
-                click.echo("Deploying Chainlink Market")
-                overlay_v1_factory_contract.deployMarket(
-                    overlay_v1_chainlink_feed_factory_contract_address,feed_contract_address,risk_parameters,
-                    {"from": dev, "maxFeePerGas": 3674000000})
+                    # deploy market
+                    click.echo("Deploying Chainlink Market")
+                    overlay_factory_contract.deployMarket(
+                        overlay_chainlink_feed_factory_contract_address,feed_contract_address,risk_parameters,
+                        {"from": dev, "maxFeePerGas": 3674000000})
 
-                click.echo(f"Market deployed [{market}]")
-            
-                all_feeds_all_parameters[key][chain_key][feed_oracle]['market'] = f'{market}'
-                data = all_feeds_all_parameters
-                OM.update_feeds_with_market_parameter(data)
+                    click.echo(f"Market deployed [{market}]")
+                
+                    all_feeds_all_parameters[key][chain_key][feed_oracle]['market'] = f'{market}'
+                    data = all_feeds_all_parameters
+                    OM.update_feeds_with_market_parameter(data)
             
