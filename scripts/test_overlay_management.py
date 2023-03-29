30fdd06547e898040d08ab540c467e055ce2f10e
deepsp94 <anantsp94@gmail.com>
2023-03-29 22:29:25 +0530

    Test `update_all_parameters()`

--- scripts/test_overlay_management.py
+++ scripts/test_overlay_management.py
@@ -42,6 +42,31 @@ class TestOM(unittest.TestCase):
         expected_result = self.get_mock_dict()
         self.assertEqual(result, expected_result)
 
+    @patch("overlay_management.open", mock_open(read_data=mock_data))
+    def test_update_all_parameters(self):
+        # Update the parameters for the "arbitrum_goerli" chain
+        new_params = {
+            "market1": {
+                "oracle": "new_mock_oracle",
+                "feed_parameters": {
+                    "deployable": False,
+                    "aggregator": "new_mock_aggregator"
+                },
+                "risk_parameter": {
+                    "deployable": True,
+                    "k": 456
+                }
+            }
+        }
+        OM.update_all_parameters(new_params, "arbitrum_goerli")
+
+        # Verify that the updated parameters were stored
+        expected_result = self.get_mock_dict()
+        expected_result["arbitrum_goerli"]["market1"] = new_params["market1"]
+        with patch("overlay_management.open",
+                   mock_open(read_data=json.dumps(expected_result))):
+            result = OM.get_all_parameters("arbitrum_goerli")
+            self.assertEqual(result, new_params)
 
     # def test_filter_by_network(self):
     #     chainlist = [OM.ARB_TEST, OM.ARB_MAIN]
