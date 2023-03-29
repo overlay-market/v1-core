21c8e39c0dc4a678aa63c849d0ba762988b8e054
deepsp94 <anantsp94@gmail.com>
2023-03-29 20:33:20 +0530

    Test for get_all_parameters

--- scripts/test_overlay_management.py
+++ scripts/test_overlay_management.py
@@ -1,15 +1,47 @@
 import unittest
-from overlay_management import OM 
+from unittest.mock import mock_open, patch
+import json
+from overlay_management import OM
 
 class TestRiskParams(unittest.TestCase):
+    
+    mock_data = '''
+        {
+            "arbitrum_goerli": {
+                "market1": {
+                    "oracle": "mock_oracle",
+                    "feed_parameters": {
+                        "deployable": true,
+                        "aggregator": "mock_aggregator"
+                        },
+                    "risk_parameter": {
+                        "deployable": false,
+                        "k": 123
+                    }
+                }
+            }
+        }
+    '''
 
+    def get_mock_dict(self):
+        return json.loads(self.mock_data)
+    
     def test_risk_param_order(self): #the order is important, so we test it
         risk_params = ["k", "lambda", "delta", "capPayoff", "capNotional", "capLeverage", "circuitBreakerWindow", "circuitBreakerMintTarget", "maintenanceMarginFraction", "maintenanceMarginBurnRate", "liquidationFeeRate", "tradingFeeRate", "minCollateral", "priceDriftUpperLimit", "averageBlockTime"]
         self.assertListEqual(OM.risk_params, risk_params)
-
+    
+    @patch("overlay_management.open", mock_open(read_data=mock_data))
     def test_get_all_parameters(self):
-        ap = OM.get_all_parameters()
-        ...
+        # Test with chain_id as input
+        result = OM.get_all_parameters("arbitrum_goerli")
+        expected_result = self.get_mock_dict()["arbitrum_goerli"]
+        self.assertEqual(result, expected_result)
+
+        # Test with no input
+        result = OM.get_all_parameters()
+        expected_result = self.get_mock_dict()
+        self.assertEqual(result, expected_result)
+
 
     # def test_filter_by_network(self):
     #     chainlist = [OM.ARB_TEST, OM.ARB_MAIN]
