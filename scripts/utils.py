58a756cd9a9153873e5e3f8bd29e0b3362d467b6
deepsp94 <anantsp94@gmail.com>
2023-04-27 23:34:36 +0530

    Re-add utils

---
+++ scripts/utils.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from scripts.overlay_management import OM
+
+
+def get_abi(chain_id, address):
+    if chain_id == OM.ARB_TEST:
+        api = 'api-goerli.arbiscan'
+    elif chain_id == OM.ETH_MAIN:
+        api = 'api.etherscan'
+    elif chain_id == OM.ARB_MAIN:
+        api = 'api.arbiscan'
+    elif chain_id == OM.ETH_TEST:
+        api = 'api-goerli.etherscan'
+
+    url = f'https://{api}.io/api?module=contract&action=getabi&address={address}'
+
+    response = requests.get(url)
+    abi = response.json()['result']
+    abi = json.loads(abi)
+
+    return abi
