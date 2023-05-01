fd6ca10d7b53da3a8dfbecfe54e2af02864d6fef
deepsp94 <anantsp94@gmail.com>
2023-05-01 14:02:06 +0530

    Script based on new structure of all params

--- scripts/deploy/deploy_factory.py
+++ scripts/deploy/deploy_factory.py
@@ -1,37 +1,36 @@
 from eth_utils import keccak
+from eth_abi import encode
 from ape_safe import ApeSafe
 from scripts.overlay_management import OM
 from scripts import utils
 from brownie import (
-    OverlayV1ChainlinkFeedFactory,
     Contract, history,
     accounts, network
 )
 import json
-import web3
+from web3 import Web3
 
 def main(chain_id):
     # TODO: This is deprecated and needs to be redone completely
     """
     Deploys a new Feed Factory contract
     """
-    all_params = OM.get_all_parameters(chain_id)
     safe = ApeSafe(OM.const_addresses[chain_id][OM.PROTOCOL_SAFE])
-
-    create = Contract('0x7cbB62EaA69F79e6873cD1ecB2392971036cFAa4')
-    init_code = OverlayV1ChainlinkFeedFactory.bytecode
-    tx = create.performCreate(0, init_code, {'from': safe.address})
-
-    feed_factory = OverlayV1ChainlinkFeedFactory.deploy(
-        600, 3600, {"from": safe.address})
-
-    factory_addr = OM.const_addresses[chain_id]['factory']
-    factory_abi = utils.get_abi(chain_id, factory_addr)
-    factory = Contract.from_abi('factory', factory_addr, factory_abi)
-
-    # Add factory
-    factory.addFeedFactory(feed_factory, {"from": safe.address})
-    safe_tx = safe.multisend_from_receipts()
-    safe.sign_transaction(safe_tx)
-    safe.post_transaction(safe_tx)
-    breakpoint()
+    deployable_ff = OM.get_deployable_feed_factory(chain_id)
+    
+    for ff in deployable_ff:
+        # Load the contract using brownie and get contract object from `locals()`
+        exec(f"from brownie import {ff['contract_name']}")
+        ff_contract = locals()[ff['contract_name']]
+        # Encode contract constructor arguments
+        abi = ff_contract.abi
+        args_constructor = abi[0]['inputs']
+        types_constructor = [ca['type'] for ca in args_constructor]
+        encoded_constructor = encode(
+            types_constructor,
+            list(ff['feed_factory_parameters'].values())
+        )
+        hex_constructor = Web3.toHex(encoded_constructor)[2:]
+        # Get contract bytecode and append encoded constructors
+        bytecode = ff_contract.bytecode
+        data = bytecode + hex_constructor
