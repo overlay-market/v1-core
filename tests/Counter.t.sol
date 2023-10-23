79ac12676057afd39cc9a92ce1b936c66616db2a
Ezequiel <eperez@itba.edu.ar>
2023-10-23 16:18:43 -0300

    fix: remappings

--- tests/Counter.t.sol
+++ tests/Counter.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Counter} from "../contracts/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
