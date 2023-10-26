3481d2b355a83dcabff20453357f7f74ca7b5a9f
deepsp94 <anantsp94@gmail.com>
2023-10-25 21:27:41 -0400

    Deploy OVL token with safe and EOA

--- scripts/deploy/deploy_token.py
+++ scripts/deploy/deploy_token.py
@@ -1,6 +1,30 @@
 from ape_safe import ApeSafe
-from brownie import network, accounts
+import web3
+from brownie import accounts, OverlayV1Token
 from scripts.overlay_management import OM
+from scripts import utils
+
+
+MINTER_ROLE = web3.solidityKeccak(['string'], ["MINTER"])
+
+
+def deploy_w_eoa(eoa):
+    ovl = eoa.deploy(OverlayV1Token)
+    ovl.grantRole(MINTER_ROLE, eoa, {"from": eoa})
+    return ovl
+
+
+def deploy_w_safe(chain_id, safe):
+    # Get contract bytecode
+    data = OverlayV1Token.bytecode
+
+    # Load create call contract
+    create_call = utils.load_const_contract(chain_id, OM.CREATE_CALL)
+
+    # Deploy feed factory and get address
+    tx = create_call.performCreate(0, data, {'from': safe.address})
+    ovl_address = tx.events['ContractCreation']['newContract']
+    return ovl_address
 
 
 def main(chain_id):
@@ -9,8 +33,10 @@ def main(chain_id):
     all_params = OM.get_all_parameters(chain_id)
     if chain_id == OM.ARB_TEST:
         eoa_name = input('Enter EOA name: ')
-        # EOA will be called safe for consistency
-        safe = accounts.load(eoa_name)
+        eoa = accounts.load(eoa_name)
+        ovl = deploy_w_eoa(eoa)
+        ovl_addr = ovl.address
     else:
         safe = ApeSafe(OM.const_addresses[chain_id][OM.PROTOCOL_SAFE])
-
+        ovl_addr = deploy_w_safe(chain_id, all_params, safe)
+    print(f'Overlay token deployed at {ovl_addr}')
