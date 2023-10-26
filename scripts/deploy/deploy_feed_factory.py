3684fe1b3532e32a76b539abc0bfe0c84867f6d5
deepsp94 <anantsp94@gmail.com>
2023-10-26 17:59:20 -0400

    Deploy feed factory with EOA

--- scripts/deploy/deploy_feed_factory.py
+++ scripts/deploy/deploy_feed_factory.py
@@ -2,31 +2,29 @@ from eth_abi import encode
 from ape_safe import ApeSafe
 from scripts.overlay_management import OM
 from scripts import utils
-from brownie import history
+from brownie import history, accounts
 import json
 from web3 import Web3
 
 
-def main(chain_id):
-    """
-    Deploys a new Feed Factory contract
-    """
-    print("Commence feed factory deployment script")
-    safe = ApeSafe(OM.const_addresses[chain_id][OM.PROTOCOL_SAFE])
-    all_params = OM.get_all_parameters(chain_id)
-    deployable_ff = OM.get_deployable_feed_factory(chain_id)
-    num_to_deploy = len(deployable_ff)
-    print(f'Feed factories to deploy: {num_to_deploy}')
-    if num_to_deploy == 0:
-        return
+def get_feed_factory_details(key, all_params):
+    ff = all_params[key]
+    # Load contract using brownie and get contract object from `locals()`
+    exec(f"from brownie import {ff['contract_name']}")
+    ff_contract = locals()[ff['contract_name']]
+    return ff, ff_contract
 
-    for ff_key in deployable_ff:
+
+def add_feed_factory_to_factory(chain_id, gov, ff_address):
+    # Add feed factory to factory
+    factory = utils.load_const_contract(chain_id, OM.FACTORY_ADDRESS)
+    factory.addFeedFactory(ff_address, {"from": gov})
+
+
+def deploy_w_safe(chain_id, safe, dep_ff, num, all_params):
+    for ff_key in dep_ff:
         print(f'Commencing feed factory deployment: {ff_key}')
-        ff = all_params[ff_key]
-
-        # Load contract using brownie and get contract object from `locals()`
-        exec(f"from brownie import {ff['contract_name']}")
-        ff_contract = locals()[ff['contract_name']]
+        ff, ff_contract = get_feed_factory_details(ff_key, all_params)
 
         # Encode contract constructor arguments
         abi = ff_contract.abi
@@ -62,18 +60,57 @@ def main(chain_id):
             json.dump(verif_info, j, indent=4)
 
         # Add feed factory to factory
-        factory = utils.load_const_contract(chain_id, OM.FACTORY_ADDRESS)
-        factory.addFeedFactory(ff_address, {"from": safe.address})
+        add_feed_factory_to_factory(chain_id, safe.address, ff_address)
 
     # Build multisend tx
     # Two txs per feed factory: 1) Deployment; 2) Addition to factory
-    print(f'Batching {num_to_deploy*2} transactions')
+    print(f'Batching {num*2} transactions')
     hist = history.from_sender(safe.address)
-    safe_tx = safe.multisend_from_receipts(hist[-num_to_deploy*2:])
+    safe_tx = safe.multisend_from_receipts(hist[-num*2:])
 
     # Sign and post
     safe.sign_transaction(safe_tx)
     safe.post_transaction(safe_tx)
+
+    return all_params
+
+
+def deploy_w_eoa(chain_id, eoa, dep_ff, all_params):
+    for ff_key in dep_ff:
+        print(f'Commencing feed factory deployment: {ff_key}')
+        ff, ff_contract = get_feed_factory_details(ff_key, all_params)
+        # Deploy feed factory and get address
+        eoa.deploy(ff_contract, *ff['feed_factory_parameters'].values())
+        ff_address = ff_contract[-1].address
+        # Save address to dict
+        all_params[ff_key]['feed_factory_address'] = ff_address
+        # Add feed factory to factory
+        add_feed_factory_to_factory(chain_id, eoa.address, ff_address)
+    return all_params
+
+
+def main(chain_id):
+    """
+    Deploys a new Feed Factory contract
+    """
+    print("Commence feed factory deployment script")
+    OM.connect_to_chain(chain_id)
+    all_params = OM.get_all_parameters(chain_id)
+    deployable_ff = OM.get_deployable_feed_factory(chain_id)
+    num_to_deploy = len(deployable_ff)
+    if num_to_deploy == 0:
+        return
+    print(f'Feed factories to deploy: {num_to_deploy}')
+    if chain_id == OM.ARB_TEST:
+        eoa_name = input('Enter EOA name: ')
+        eoa = accounts.load(eoa_name)
+        all_params = deploy_w_eoa(chain_id, eoa, deployable_ff, all_params)
+    else:
+        safe = ApeSafe(OM.const_addresses[chain_id][OM.PROTOCOL_SAFE])
+        all_params = deploy_w_safe(
+            chain_id, safe, deployable_ff,
+            num_to_deploy, all_params
+        )
 
     # Save addresses to file
     OM.update_all_parameters(all_params, chain_id)
