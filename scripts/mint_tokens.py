4cb9a7f8c89791bccf2d5621d0b8226d2f2340a7
deepsp94 <anantsp94@gmail.com>
2023-12-05 13:31:30 -0500

    Mint and transfer to specified address

--- scripts/mint_tokens.py
+++ scripts/mint_tokens.py
@@ -1,6 +1,7 @@
 from brownie import accounts
 from scripts.overlay_management import OM
 from scripts import utils
+from brownie import web3
 
 
 def main(eoa, chain_id):
@@ -9,11 +10,16 @@ def main(eoa, chain_id):
     eoa = accounts.load(eoa)
     ovl = utils.load_const_contract(chain_id, OM.OVL_ADDRESS)
     amount = int(input('Enter amount to mint: '))
-    print(f'Minting {amount} OVL to {eoa.address}')
+    to_address = input('Enter address to mint to (leave blank if mint to gov): ')  # noqa: E501
+    if to_address == '':
+        to_address = eoa.address
+    print(f'Minting {amount} OVL to {to_address}')
     # Ask are you sure? y/n
     check = input('Are you sure? y/n: ')
     if check == 'y':
-        ovl.mint(eoa, amount * 10 ** ovl.decimals(), {"from": eoa})
+        minter_role = web3.keccak(text='MINTER')
+        ovl.grantRole(minter_role, eoa, {"from": eoa})
+        ovl.mint(to_address, amount * 10 ** ovl.decimals(), {"from": eoa})
     else:
         print('Aborting')
         return
