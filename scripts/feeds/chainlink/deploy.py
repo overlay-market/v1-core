eccff0ed2bae9d34dc5c2d4bafafb9605c793ce1
Ezequiel <eperez@itba.edu.ar>
2024-01-11 16:09:56 -0300

    feat: add create and deploy scripts for chainlink feeds

---
+++ scripts/feeds/chainlink/deploy.py
@@ -0,0 +1,26 @@
+import click
+
+from brownie import OverlayV1ChainlinkFeedFactory, accounts, network
+
+
+def main():
+    """
+    Deploys a new OverlayV1ChainlinkFeedFactory contract, which allows for
+    permissionless deployment of OverlayV1ChainlinkFeed feeds.
+    """
+    click.echo(f"You are using the '{network.show_active()}' network")
+    dev = accounts.load(click.prompt(
+        "Account", type=click.Choice(accounts.load())))
+
+    # assemble constructor params
+    params = ["microWindow (uint256)", "macroWindow (uint256)"]
+    args = [
+        click.prompt(f"{param}")
+        for param in params
+    ]
+
+    # deploy feed factory
+    feed_factory = OverlayV1ChainlinkFeedFactory.deploy(
+        *args, {"from": dev}, publish_source=True)
+    click.echo(f"Chainlink Feed Factory deployed [{feed_factory.address}]")
+    click.echo("NOTE: Feed Factory is not registered in OverlayV1Factory yet!")
