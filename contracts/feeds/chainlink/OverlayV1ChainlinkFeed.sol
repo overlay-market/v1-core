efee1332fc021bb77d0bb9f687acdc9501873d0d
Erik <esnetto@gmail.com>
2024-01-18 19:38:42 +0000

    Update import path for OverlayV1Feed in OverlayV1ChainlinkFeed

--- contracts/feeds/chainlink/OverlayV1ChainlinkFeed.sol
+++ contracts/feeds/chainlink/OverlayV1ChainlinkFeed.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.10;
 
-import "../OverlayV1Feed.sol";
+import "contracts/feeds/OverlayV1Feed.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract OverlayV1ChainlinkFeed is OverlayV1Feed {
