53a556ef13d7d073b91b9e988fa67dbf6d2eb6de
Ezequiel <eperez@itba.edu.ar>
2024-02-03 06:18:21 +0000

    feat(Echidna): add unwinds and liquidations to test environment

--- tests/invariants/MarketEchidnaAdvanced.t.sol
+++ tests/invariants/MarketEchidnaAdvanced.t.sol
@@ -11,11 +11,14 @@ import {TestUtils} from "./TestUtils.sol";
 // run from base project directory with:
 // echidna tests/invariants/MarketEchidnaAdvanced.t.sol --contract MarketEchidnaAdvanced --config tests/invariants/MarketEchidnaAdvanced.yaml
 contract MarketEchidnaAdvanced is MarketEchidna {
+    // ghost variables used to verify invariants
+    mapping(address => uint256[]) positions; // owner => positionIds
+
     // Handler functions to "guide" the stateful fuzz test
     // Note: handlers have to be public in order to be called by Echidna
 
     // wrapper around market.build()
-    function buildWrapper(bool isLong, uint256 collateral) public returns (uint256) {
+    function buildWrapper(bool isLong, uint256 collateral) public returns (uint256 posId) {
         // bound collateral to avoid reverts
         collateral = TestUtils.clampBetween(collateral, MIN_COLLATERAL, CAP_NOTIONAL);
 
@@ -23,18 +26,56 @@ contract MarketEchidnaAdvanced is MarketEchidna {
         ovl.mint(msg.sender, collateral);
         hevm.prank(msg.sender);
 
-        return market.build({
+        posId = market.build({
             collateral: collateral,
             leverage: 1e18,
             isLong: isLong,
             priceLimit: isLong ? type(uint256).max : 0
         });
+
+        // save positions built by the sender to use in other handlers
+        positions[msg.sender].push(posId);
     }
 
     function setPrice(uint256 price) public {
         // bound market price to a reasonable range
         price = TestUtils.clampBetween(price, 1, 1e29);
         feed.setPrice(price);
+    }
+
+    function unwind(uint256 fraction) public {
+        fraction = TestUtils.clampBetween(fraction, 1e14, 1e18);
+
+        uint256[] storage posIds = positions[msg.sender];
+        require(posIds.length > 0, "No positions to unwind");
+        uint256 posId = posIds[posIds.length - 1];
+
+        (,,,,bool isLong,,,) = market.positions(keccak256(abi.encodePacked(msg.sender, posId)));
+
+        hevm.prank(msg.sender);
+        market.unwind(posId, fraction, isLong ? 0 : type(uint256).max);
+
+        (,,,,,,,uint16 fractionRemaining) = market.positions(keccak256(abi.encodePacked(msg.sender, posId)));
+
+        // remove the position if fully unwound
+        if (fractionRemaining == 0) posIds.pop();
+    }
+
+    function liquidate() public {
+        uint256[] storage posIds = positions[msg.sender];
+        require(posIds.length > 0, "No positions to unwind");
+        uint256 posId = posIds[posIds.length - 1];
+
+        (,,,,bool isLong,,,) = market.positions(keccak256(abi.encodePacked(msg.sender, posId)));
+
+        // adjust the price so that position becomes liquidatable
+        setPrice(isLong ? feed.price() / 2 : feed.price() * 2);
+
+        // liquidate the position
+        market.liquidate(msg.sender, posId);
+
+        // remove the liquidated position
+        posIds.pop();
     }
 
     // Helper functions
@@ -78,6 +119,7 @@ contract MarketEchidnaAdvanced is MarketEchidna {
         (uint256 oiBefore, uint256 oiSharesBefore) = _getOiAndShares(isLong);
 
         // unwind the whole position
+        hevm.prank(msg.sender);
         market.unwind(posId, 1e18, isLong ? 0 : type(uint256).max);
 
         (uint256 oiAfter, uint256 oiSharesAfter) = _getOiAndShares(isLong);
@@ -97,10 +139,9 @@ contract MarketEchidnaAdvanced is MarketEchidna {
         (uint256 oiBefore, uint256 oiSharesBefore) = _getOiAndShares(isLong);
 
         // adjust the price so that position becomes liquidatable
-        uint256 originalPrice = feed.price();
-        feed.setPrice(isLong ? originalPrice / 2 : originalPrice * 2);
+        feed.setPrice(isLong ? feed.price() / 2 : feed.price() * 2);
 
-        // liquidate the position
+        // liquidate the position (don't need to prank the sender here)
         market.liquidate(msg.sender, posId);
 
         (uint256 oiAfter, uint256 oiSharesAfter) = _getOiAndShares(isLong);
